9ee79106d3a12f7d8f0eb7a19b7ddf63a116f322
khanhhung <khanhhungbadong@gmail.com>
2026-03-17 16:19:39 +0700

    Fix conflict index.html and update project structure

--- Backend/src/helpers/http.ts
+++
@@ -1,9 +0,0 @@
-import type { Response } from "express";
-
-export function ok<T>(res: Response, data: T) {
-  return res.status(200).json({ ok: true, data });
-}
-
-export function created<T>(res: Response, data: T) {
-  return res.status(201).json({ ok: true, data });
-}
--- Backend/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import postRoutes from "./post.routes.js";
-
-const router = Router();
-
-router.use("/posts", postRoutes);
-
-export default router;
